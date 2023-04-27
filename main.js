110a87430498195af0b9ec593b57d44a2c2a4c27
ThiagoAlmarazz21 <115847063+ThiagoAlmarazz21@users.noreply.github.com>
2023-04-27 15:26:37 -0300

    add hero

--- main.js
+++ main.js
@@ -1,63 +1,112 @@
-const API_URL_RANDOM = 'https://api.thecatapi.com/v1/images/search?limit=2&api_key=live_aetAmX8Db5hHA5UIrsklk0LPP8MdrexFkK4VMwRM3UxlxXiVy1VRem1Y0XUKvJcH';
-const API_URL_FAVORITES = 'https://api.thecatapi.com/v1/favourites?limit=2&api_key=live_aetAmX8Db5hHA5UIrsklk0LPP8MdrexFkK4VMwRM3UxlxXiVy1VRem1Y0XUKvJcH';
+const API_URL_RANDOM = 'https://api.thecatapi.com/v1/images/search?limit=2';
+const API_URL_FAVOTITES = 'https://api.thecatapi.com/v1/favourites';
+const API_URL_FAVOTITES_DELETE = (id) => `https://api.thecatapi.com/v1/favourites/${id}`;
 
-const spanError = document.getElementById('error');
-const paraError = document.querySelector('.randoms-cats')
+const API_KEY = 'live_dAC5pYW8RKo61DifwfduOeyDv95rvy2Hen2TkK3W7bKXw4ATVdwsBdEa2lV5ZrW9';
 
-async function loadRandomCats() {
+const spanError = document.getElementById('error')
+
+async function loadRandomMichis() {
   const res = await fetch(API_URL_RANDOM);
   const data = await res.json();
- 
-  console.log('myCat');
-  console.log(data);
+  console.log('Random')
+  console.log(data)
 
-  if(res.status !== 200) {
-    spanError.innerHTML = `
-      <h2>Hubo un error: ${res.status}</h2>
-      <img width="400px" height="300px" src="https://http.cat/401"></img>
-    `;
-    paraError.classList.add('none')
-  } else{
-    const img1 = document.getElementById('img1')
-    const img2 = document.getElementById('img2')
-  
+  if (res.status !== 200) {
+    spanError.innerHTML = "Hubo un error: " + res.status;
+  } else {
+    const img1 = document.getElementById('img1');
+    const img2 = document.getElementById('img2');
+    const btn1 = document.getElementById('btn1');
+    const btn2 = document.getElementById('btn2');
+    
     img1.src = data[0].url;
     img2.src = data[1].url;
+
+    btn1.onclick = () => saveFavouriteMichi(data[0].id);
+    btn2.onclick = () => saveFavouriteMichi(data[1].id);
   }
 }
-loadRandomCats();
 
-async function loadFavoritesCats() {
-  const res = await fetch(API_URL_FAVORITES);
+async function loadFavouriteMichis() {
+  const res = await fetch(API_URL_FAVOTITES, {
+    method: 'GET',
+    headers: {
+      'X-API-KEY': API_KEY,
+    }
+  });
   const data = await res.json();
+  console.log('Favoritos')
+  console.log(data)
 
-  console.log('favoritesCats');
-  console.log(data);
+  if (res.status !== 200) {
+    spanError.innerHTML = "Hubo un error: " + res.status + data.message;
+  } else {
+    const section = document.getElementById('favorites-cats')
+    section.innerHTML = "";
 
-  if(res.status !== 200) {
-    spanError.innerHTML = 'Hubo un error: ' + res.status;
+    const h2 = document.createElement('h2');
+    const h2Text = document.createTextNode('Michis favoritos');
+    h2.appendChild(h2Text);
+    section.appendChild(h2);
+
+    data.forEach(michi => {
+      const article = document.createElement('article');
+      const img = document.createElement('img');
+      const btn = document.createElement('button');
+      const btnText = document.createTextNode('Sacar al michi de favoritos');
+
+      img.src = michi.image.url;
+      img.width = 150;
+      btn.appendChild(btnText);
+      btn.onclick = () => deleteFavouriteMichi(michi.id);
+      article.appendChild(img);
+      article.appendChild(btn);
+      section.appendChild(article);
+    });
   }
 }
-loadFavoritesCats();
 
-async function saveFavouriteCats() {
-  const res = await fetch(API_URL_FAVORITES, {
+async function saveFavouriteMichi(id) {
+  const res = await fetch(API_URL_FAVOTITES, {
     method: 'POST',
     headers: {
-      'Content-Type': 'application/json'
-  },
+      'Content-Type': 'application/json',
+      'X-API-KEY': API_KEY,
+    },
     body: JSON.stringify({
-      image_id: 'al0'
+      image_id: id
     }),
   });
   const data = await res.json();
 
-  console.log('Save');
-  console.log(res);
+  console.log('Save')
+  console.log(res)
 
-  if(res.status !== 200) {
-    spanError.innerHTML = 'Hubo un error: ' + res.status;
+  if (res.status !== 200) {
+    spanError.innerHTML = "Hubo un error: " + res.status + data.message;
+  } else {
+    console.log('Michi guardado en favoritos')
+    loadFavouriteMichis();
   }
 }
 
+async function deleteFavouriteMichi(id) {
+  const res = await fetch(API_URL_FAVOTITES_DELETE(id), {
+    method: 'DELETE',
+    headers: {
+      'X-API-KEY': API_KEY,
+    }
+  });
+  const data = await res.json();
 
+  if (res.status !== 200) {
+    spanError.innerHTML = "Hubo un error: " + res.status + data.message;
+  } else {
+    console.log('Michi eliminado de favoritos')
+    // loadFavouriteMichis();
+  }
+}
+
+loadRandomMichis();
+loadFavouriteMichis();
